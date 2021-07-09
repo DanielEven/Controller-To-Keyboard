faeeea84c64d8ec643f94e0e0b4dabf39311f5ec
Daniel Even <danieleven@mail.tau.ac.il>
2021-07-09 20:00:32 +0300

    Added a functinon to press multiple keys.

--- src/KeyboardManager.cpp
+++ src/KeyboardManager.cpp
@@ -1,6 +1,6 @@
 #include "KeyboardManager.hpp"
 
-bool pressKey(long key)
+bool pressKey(int key)
 {
     INPUT inputs[2] = {};
     ZeroMemory(inputs, sizeof(inputs));
@@ -15,4 +15,28 @@ bool pressKey(long key)
     UINT sent = SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
 
     return sent == ARRAYSIZE(inputs);
+}
+
+bool pressMultipleKeys(std::vector<int> keys)
+{
+    const int arr_size = keys.size() * 2;
+
+    INPUT *inputs = new INPUT[arr_size];
+    ZeroMemory(inputs, arr_size * sizeof(INPUT));
+
+    for (int i = 0; i < keys.size(); i++)
+    {
+        // Press
+        inputs[i].type = INPUT_KEYBOARD;
+        inputs[i].ki.wVk = keys.at(i);
+
+        // key Up
+        inputs[arr_size - i - 1].type = INPUT_KEYBOARD;
+        inputs[arr_size - i - 1].ki.wVk = keys.at(i);
+        inputs[arr_size - i - 1].ki.dwFlags = KEYEVENTF_KEYUP;
+    }
+
+    UINT sent = SendInput(arr_size, inputs, sizeof(INPUT));
+
+    return sent == arr_size;
 }
--- src/KeyboardManager.hpp
+++ src/KeyboardManager.hpp
@@ -1,6 +1,13 @@
 #include <windows.h>
+#include <algorithm>
+#include <vector>
 
 #pragma comment(lib, "user32.lib")
 
-// A function that simulates a press on one key
-bool pressKey(long key);
+// A function that simulates a press on one key.
+bool pressKey(int key);
+
+// A function that simulates a press on multiple keys.
+// The function simulates a press on the keys from the array, and then releases all of them.
+// the keys are pressed according to the order of the array.
+bool pressMultipleKeys(std::vector<int> keys);
