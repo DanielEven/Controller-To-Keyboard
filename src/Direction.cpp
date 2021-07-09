d5d9bc9ea9f1753507bfd79ed7761eedd4279c76
Daniel Even <danieleven@mail.tau.ac.il>
2021-07-09 20:17:58 +0300

    Added a direction class.

---
+++ src/Direction.cpp
@@ -0,0 +1,10 @@
+#include "Direction.hpp"
+#include <stdexcept>
+
+Direction::Direction(int dx, int dy):Point(dx,dy)
+{
+    if (dx > 1 || -1 > dx || dy > 1 || -1 > dy)
+    {
+        throw std::invalid_argument("The x and y values should be between -1 and 1.");
+    }
+}
---
+++ src/Direction.hpp
@@ -0,0 +1,7 @@
+#include "Point.hpp"
+
+class Direction : Point
+{
+public:
+    Direction(int dx, int dy);
+};
