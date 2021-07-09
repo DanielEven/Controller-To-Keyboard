aaa2d798d65b6f08e608fc51492509f4750dc8cb
Daniel Even <danieleven@mail.tau.ac.il>
2021-07-09 16:02:42 +0300

    Added point printing option.

--- src/Point.cpp
+++ src/Point.cpp
@@ -1,7 +1,15 @@
 #include "Point.hpp"
 
+using namespace std;
+
 Point::Point(int nx, int ny)
 {
     this->x = nx;
     this->y = ny;
+}
+
+// Printing a Point.
+std::ostream &operator<<(std::ostream &o, Point const *p)
+{
+    return o << "(" << p->x << ", " << p->y << ")";
 }
--- src/Point.hpp
+++ src/Point.hpp
@@ -1,7 +1,13 @@
+#include <iostream>
+using namespace std;
+
 class Point
 {
 public:
     int x, y;
 
     Point(int nx, int ny);
+
+    // Printing a Point.
+    friend std::ostream &operator<<(std::ostream &o, Point const *p);
 };
