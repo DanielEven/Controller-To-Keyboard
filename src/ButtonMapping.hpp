a203b19d2d0d34a3b95e4b20773c341022fb855e
Daniel Even <danieleven@mail.tau.ac.il>
2021-07-09 22:55:34 +0300

    Added stick mapping.

--- src/ButtonMapping.hpp
+++ src/ButtonMapping.hpp
@@ -20,3 +20,19 @@ std::map<WORD, int> button_mapping = {
     //{XINPUT_GAMEPAD_X, 1},
     //{XINPUT_GAMEPAD_Y, 1}
 };
+
+std::map<int, int> left_stick_x_mapping = {
+    {1, VK_RIGHT},
+    {-1, VK_LEFT}};
+
+std::map<int, int> left_stick_y_mapping = {
+    {1, -1},
+    {-1, -1}};
+
+std::map<int, int> right_stick_x_mapping = {
+    {1, -1},
+    {-1, -1}};
+
+std::map<int, int> right_stick_y_mapping = {
+    {1, -1},
+    {-1, -1}};
