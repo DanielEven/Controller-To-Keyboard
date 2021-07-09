3ab2471d7eb98ceff00d9d55c966c333c4ce249f
Daniel Even <danieleven@mail.tau.ac.il>
2021-07-09 22:49:51 +0300

    Added the button mapping.

---
+++ src/ButtonMapping.hpp
@@ -0,0 +1,22 @@
+#include "KeyboardManager.hpp"
+#include "Controller.hpp"
+
+#include <map>
+
+// this is a map between the controller buttons and the keyboard buttons.
+std::map<WORD, int> button_mapping = {
+    {XINPUT_GAMEPAD_DPAD_UP, 'W'},
+    {XINPUT_GAMEPAD_DPAD_DOWN, 'S'},
+    {XINPUT_GAMEPAD_DPAD_LEFT, 'A'},
+    {XINPUT_GAMEPAD_DPAD_RIGHT, 'D'},
+    //{XINPUT_GAMEPAD_START, -1},
+    //{XINPUT_GAMEPAD_BACK, -1},
+    //{XINPUT_GAMEPAD_LEFT_THUMB, 1},
+    //{XINPUT_GAMEPAD_RIGHT_THUMB, 1},
+    //{XINPUT_GAMEPAD_LEFT_SHOULDER, 1},
+    //{XINPUT_GAMEPAD_RIGHT_SHOULDER, 1},
+    {XINPUT_GAMEPAD_A, VK_DOWN},
+    //{XINPUT_GAMEPAD_B, 1},
+    //{XINPUT_GAMEPAD_X, 1},
+    //{XINPUT_GAMEPAD_Y, 1}
+};
