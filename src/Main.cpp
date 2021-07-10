437c2fb86cfab04adf2949d8983164e4f9957a47
Daniel Even <danieleven@mail.tau.ac.il>
2021-07-10 19:31:28 +0300

    Implemented the keyboard updating according to the controller's sticks.

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,7 +4,22 @@
 #include <set>
 
 void updateButtons(Controller *c);
+void updateSticks(Controller *c);
 
+int main()
+{
+    Controller *c = new Controller();
+
+    while (!c->isButtonPressed(XINPUT_GAMEPAD_BACK))
+    {
+        updateButtons(c);
+        updateSticks(c);
+    }
+
+    return 0;
+}
+
+// A map of the pressed buttons.
 std::map<WORD, bool> pressed_mapping = {
     {XINPUT_GAMEPAD_DPAD_UP, false},
     {XINPUT_GAMEPAD_DPAD_DOWN, false},
@@ -21,63 +36,161 @@ std::map<WORD, bool> pressed_mapping = {
     {XINPUT_GAMEPAD_X, false},
     {XINPUT_GAMEPAD_Y, false}};
 
-int main()
-{
-    Controller *c = new Controller();
-
-    updateButtons(c);
-
-    return 0;
-}
-
 /*************************
     This function updates the keyboard according to the controller's buttons.
 
     Steps:
     1. Create the current button set.
-    2. Checking if there are released buttons.
-    3. Pressing the new buttons.
+    2. Checking if there are released buttons, and releasing the keys.
+    3. Pressing the new keys.
 *************************/
 void updateButtons(Controller *c)
 {
-    while (!c->isButtonPressed(XINPUT_GAMEPAD_BACK))
+
+    // 1
+    std::set<WORD, greater<WORD>> current;
+
+    for (auto iter = button_mapping.begin(); iter != button_mapping.end(); iter++)
     {
-        // 1
-        std::set<WORD, greater<WORD>> current;
-
-        for (auto iter = button_mapping.begin(); iter != button_mapping.end(); iter++)
+        WORD button = iter->first;
+        if (c->isButtonPressed(button))
         {
-            WORD button = iter->first;
-            if (c->isButtonPressed(button))
+            current.insert(button);
+        }
+    }
+
+    //2
+    for (auto iter = pressed_mapping.begin(); iter != pressed_mapping.end(); iter++)
+    {
+        WORD button = iter->first;
+        if (pressed_mapping[button] == true) // This is a pressed button.
+        {
+            if (current.count(button) == 0)
             {
-                current.insert(button);
+                // We stopped pressing the button.
+                releaseKey(button_mapping[button]);
+                pressed_mapping[button] = false;
             }
         }
+    }
 
-        //2
-        for (auto iter = pressed_mapping.begin(); iter != pressed_mapping.end(); iter++)
+    // 3
+    for (auto iter = current.begin(); iter != current.end(); iter++)
+    {
+        WORD button = (*iter);
+        if (pressed_mapping[button] == false) // if the button isn't already pressed.
         {
-            WORD button = iter->first;
-            if (pressed_mapping[button] == true) // This is a pressed button.
+            holdKey(button_mapping[button]);
+            pressed_mapping[button] = true;
+        }
+    }
+}
+
+// A map of the moving sticks.
+/*
+L - Left
+R - Right
+*/
+std::map<char, bool> moving_sticks_mapping = {
+    {'L', false},
+    {'R', false},
+};
+
+/*************************
+    This function updates the keyboard according to the controller's sticks.
+
+    Steps:
+    1. Create the current moving sticks set.
+    2. Checking if there are released sticks, and releasing the keys.
+    3. Pressing the new keys.
+*************************/
+void updateSticks(Controller *c)
+{
+    const Direction *origin = new Direction(0, 0);
+
+    // 1
+    std::set<char, greater<char>> current;
+
+    for (auto iter = moving_sticks_mapping.begin(); iter != moving_sticks_mapping.end(); iter++)
+    {
+        char stick = iter->first;
+        Direction *dir;
+        switch (stick)
+        {
+        case 'R':
+            dir = c->getRightStickDirection();
+            break;
+        case 'L':
+            dir = c->getLeftStickDirection();
+            break;
+        }
+        if (!(*dir == *origin))
+        {
+            current.insert(stick);
+        }
+    }
+
+    //2
+    for (auto iter = moving_sticks_mapping.begin(); iter != moving_sticks_mapping.end(); iter++)
+    {
+        char stick = iter->first;
+        if (moving_sticks_mapping[stick] == true) // This is a moving stick.
+        {
+            if (current.count(stick) == 0)
             {
-                if (current.count(button) == 0)
+                // We stopped moving the stick.
+
+                Direction *dir;
+                switch (stick)
                 {
-                    // We stopped pressing the button.
-                    releaseKey(button_mapping[button]);
-                    pressed_mapping[button] = false;
+                case 'R':
+                    dir = c->getRightStickDirection();
+                    if (dir->x != 0)
+                        releaseKey(right_stick_x_mapping[dir->x]);
+                    if (dir->y != 0)
+                        releaseKey(right_stick_y_mapping[dir->y]);
+                    break;
+                case 'L':
+                    dir = c->getLeftStickDirection();
+                    if (dir->x != 0)
+                        releaseKey(left_stick_x_mapping[dir->x]);
+                    if (dir->y != 0)
+                        releaseKey(left_stick_y_mapping[dir->y]);
+                    break;
                 }
+
+                moving_sticks_mapping[stick] = false;
             }
         }
+    }
 
-        // 3
-        for (auto iter = current.begin(); iter != current.end(); iter++)
+    // 3
+    for (auto iter = current.begin(); iter != current.end(); iter++)
+    {
+        char stick = (*iter);
+        if (moving_sticks_mapping[stick] == false) // if the button isn't already pressed.
         {
-            WORD button = (*iter);
-            if (pressed_mapping[button] == false) // if the button isn't already pressed.
+
+            Direction *dir;
+            switch (stick)
             {
-                holdKey(button_mapping[button]);
-                pressed_mapping[button] = true;
+            case 'R':
+                dir = c->getRightStickDirection();
+                if (dir->x != 0)
+                    holdKey(right_stick_x_mapping[dir->x]);
+                if (dir->y != 0)
+                    holdKey(right_stick_y_mapping[dir->y]);
+                break;
+            case 'L':
+                dir = c->getLeftStickDirection();
+                if (dir->x != 0)
+                    holdKey(left_stick_x_mapping[dir->x]);
+                if (dir->y != 0)
+                    holdKey(left_stick_y_mapping[dir->y]);
+                break;
             }
+
+            moving_sticks_mapping[stick] = true;
         }
     }
 }
